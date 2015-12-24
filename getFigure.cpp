b3ff7beade418d31450a8b5e4aea4cb4eb4fd84c
GenSyhius <sunyuanhao123456@gmail.com>
2015-12-24 10:41:32 +0800

    the matrix can store road length

--- getFigure.cpp
+++ getFigure.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <string>
 #include "Matrix.h"
-#include "Matrix.cpp"
 
 using namespace std;
 
 Matrix getFigure() {
 	int nodeNumber = 0;
 	cout << "How many node in the figure ?" << endl
-		 << "Node Number? ";// enter the number of the Scenic Spot
+		 << "Number? ";// enter the number of the Scenic Spot
 	cin >> nodeNumber;	
 
 	Matrix newFigure(nodeNumber,nodeNumber);// A new figure to store the road
 
 	int flag = 0;// using in "while", judging whether input or not
 	while(flag == 0) {
-		char temp[5];
+		char temp[5];// Store node data
+		int length = 0;// Store road length
 		cout << "Enter the two node, example(<1,2>)" << endl 
 			 << "?";// enter the two node (if two node linked)
 		cin >> temp;
 
+		cout << "Enter the road length: " << endl
+			 << "?";
+		cin  >> length;
+
 		int temp1 = (int)(temp[1] - '0');
 		int temp2 = (int)(temp[3] - '0');
 
-		++newFigure[temp1][temp2];		
+		newFigure[temp1][temp2] = length;		
 
 		cout << "Do you want enter new? (enter 0 for next, enter 1 for exit)" << endl 
 		     << "?";
@@ -34,12 +38,12 @@ Matrix getFigure() {
 		newFigure[i][i] = 1;
 	}
 
-	for (int i = 0; i < nodeNumber; ++i) {
-		for(int j = 0; j < nodeNumber; ++j) {
-			if (newFigure[i][j] > 1)
-				newFigure[i][j] = 1;
-		}
-	}
+	// for (int i = 0; i < nodeNumber; ++i) {
+	// 	for(int j = 0; j < nodeNumber; ++j) {
+	// 		if (newFigure[i][j] > 1)
+	// 			newFigure[i][j] = 1;
+	// 	}
+	// }
 
 	return newFigure;
 }
