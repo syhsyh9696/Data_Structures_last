68bc0984739f3b1fb0dc76fa5b9b51be0daf8e6a
GenSyhius <sunyuanhao123456@gmail.com>
2015-12-27 20:45:53 +0800

    ?wrong answer

--- readSS.cpp
+++ readSS.cpp
@@ -14,7 +14,7 @@ public:
     // ~SS_Node();
     bool getInformation();
     std::string getName() { return Name; }
-private:
+
     std::string Number;// Store scenic spot number
     std::string Name;// Store scenic spot name
     std::string Introduction;// Store scenic spot introduction
@@ -52,13 +52,27 @@ SS_Node* readSpot() {
     }// End if
 
     SS_Node* Temp = new SS_Node[MAXNUM];
+    // std::string num;
+    // std::string nam;
+    // std::string intro;
 
-        std::string num;
-        std::string nam;
-        std::string intro;
-        while (inClientFile >> num >> nam >> intro)
-            outputLine(num, nam, intro);
+    for (int i = 0; i < MAXNUM; ++i) {
+        char temp_char[256];
+        for (int j = 0; j < 4; ++j) {
+                inClientFile.getline(temp_char,256,'\n');// getline(char *, int, char)
+                
+                switch(j) {
+                    case 0:Temp[i].Number = temp_char;break;
+                    case 1:Temp[i].Name = temp_char;break;
+                    case 2:Temp[i].Introduction = temp_char;break;
+                    case 3:break;
+                }
 
+        }
+
+    }
+        
+    return Temp;
 }
 
 void outputLine(std::string a, std::string b, std::string c) {
@@ -69,6 +83,11 @@ void outputLine(std::string a, std::string b, std::string c) {
 
 int main(int argc, char const *argv[])
 {
-    readSpot();
+    SS_Node* test;
+    test = readSpot();
+
+    for (int i = 0; i < 10; ++i) {
+       std::cout << std::left << std::setw(10) << test[i].Number << std::setw(10) << test[i].Name << std::endl;
+    }
     return 0;
 }
